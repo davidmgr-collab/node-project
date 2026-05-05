50a5c843c1299e7716c07bbef43e2eb6e863d244
wallencalline <wallencalline27@gmail.com>
2026-05-05 11:50:02 +0200

    changed hard coded port to use env port with fallback 3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors=require('cors');
 require('dotenv').config();
 const studentRoutes=("./routes/students");
 const app=express();
-const port=3000;
+const port=process.env.PORT || 3000;
 app.get('/',(req,res)=>{
     console.log('hello world');
 });
